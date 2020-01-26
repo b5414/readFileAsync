647efe6060aeab62977b873dbe6321329673a089
Vadim <mr.b5414@yandex.ru>
2020-01-27 05:09:50 +0800

    Add files via upload

---
+++ javascript.js
@@ -0,0 +1,52 @@
+﻿$(()=>{
+	console.log('v');
+	
+	/*
+	// sync:
+	$('#theButton').click(()=>{
+		const file = $('#theFile')[0].files[0];
+		if(!file)return;
+		const reader = new FileReader();
+		reader.onload=()=>{
+			const text = reader.result;
+			console.log(text);
+			$('#theText').val(text);
+		};
+		reader.onerror=()=>{
+			console.log(reader.error);
+		};
+		reader.readAsText(file);
+	});
+	*/
+	
+	// async:
+	const readAsync=(file)=>new Promise((r, q)=>{
+		const reader = new FileReader();
+		reader.onload=()=>{
+			r(reader.result);
+		};
+		reader.onerror=()=>{
+			console.log(reader.error);
+			q();
+		};
+		reader.readAsText(file);
+	});
+	
+	$('#theButton').click(async()=>{
+		const file = $('#theFile')[0].files[0];
+		if(!file)return;
+		const text = await readAsync(file);
+		console.log(text);
+		$('#theText').val(text);
+	});
+});
+
+
+
+
+
+
+
+
+
+
